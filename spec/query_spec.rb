4fd278b097da10bb90b347a9e003740ff26750a9
Felipe Nakandakari <fenak@boo-box.com>
2011-03-28 11:31:23 -0300

    [spec] should execute query

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -1,8 +1,18 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Query" do
+  it "should execute query" do
 
-  it "should execute query"
+    successful_response = {"data"=>{"kind"=>"bigquery#queryResults", "fields"=>[{"id"=>"views", "type"=>"integer"}, {"id"=>"clicks", "type"=>"integer"}]}}
+
+    query = BQ::Query.new
+    query.should_receive(:execute).with('QUERY').and_return(successful_response)
+    result = query.execute('QUERY')
+    
+    result.keys.include?('error').should be_false
+    result.keys.include?('data').should be_true
+  end
+
   it "should raise excepton if token is invalid"
   it "should return values in Result object"
 
