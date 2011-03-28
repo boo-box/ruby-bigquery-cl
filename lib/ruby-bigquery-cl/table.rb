11942afde3cc853376c14cdf949dc02ecaec35b9
Felipe Nakandakari <fenak@Fenak.local>
2011-03-28 20:54:29 -0300

    project rdoc

--- lib/ruby-bigquery-cl/table.rb
+++ lib/ruby-bigquery-cl/table.rb
@@ -6,12 +6,14 @@ module BQ
     class << self
       # Create a new table. Call this class method with the name of the table
       # and one shema array with collumns hashes following this example:
-      # shema = [
-      #  {:id" => "name", :type => "string", :mode => "REQUIRED"},
-      #  {:id" => "age", :type => "integer"},
-      #  {:id" => "weight", :type => "float"}
-      #  {:id" => "is_magic", :type => "boolean"}
-      # ]
+      #
+      #   schema = [
+      #     {:id" => "name", :type => "string", :mode => "REQUIRED"},
+      #     {:id" => "age", :type => "integer"},
+      #     {:id" => "weight", :type => "float"},
+      #     {:id" => "is_magic", :type => "boolean"}
+      #   ]
+      #
       # A hash is returned with the API response.
       def create(table, schema)
         url = "https://www.googleapis.com/bigquery/v1/tables"
