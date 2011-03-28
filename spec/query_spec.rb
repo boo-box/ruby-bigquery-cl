cd5734b1445bc32f1e56490bdec34e1d4c71d44b
Douglas R. Roeder <douglas@boo-box.com>
2011-03-28 09:41:05 -0300

    new specs

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -1,16 +1,9 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Query" do
-<<<<<<< HEAD
 
   it "should execute query"
   it "should raise excepton if token is invalid"
   it "should return values in Result object"
 
-=======
-  it "should have tests" do
-    x = Boo::Google::Query.new("23")
-    pending "write tests or I will kneecap you"
-  end 
->>>>>>> Specs for bigquery gem
 end 
