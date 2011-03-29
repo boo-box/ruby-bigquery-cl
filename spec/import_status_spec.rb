3bb8918f9141cf814ff29957d060b7b26b7e92a9
Douglas R. Roeder <douglas@boo-box.com>
2011-03-29 11:54:35 -0300

    (fix) import_status and table specs

--- spec/import_status_spec.rb
+++ spec/import_status_spec.rb
@@ -2,14 +2,13 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "ImportStatus" do
   it "should check a tables's import status" do
-    google_rpc = {
-        :method => 'bigquery.imports.get',
-        :params => {:table_name => 'examplebucket/tables/ponylist', :import_id => 'cfa2a4b3c283d357'}
-      }
     successful_return = {"data" => {"kind" => "bigquery#import","status" => "IMPORT_DONE","sources" => [{"uri" => "examplebucket/tables/pony_sourcetable.csv"}]}} 
 
+    table     = "examplebucket/tables/ponylist"
+    import_id = "cfa2a4b3c283d357"
+
     # check status
-    BQ.stub(:request).with(:post, google_rpc.to_json).and_return(successful_return)
-    import_status = BQ::ImportStatus.new("examplebucket/tables/ponylist","cfa2a4b3c283d357")
+    BQ.should_receive(:request).with(:post, {}, "https://www.googleapis.com/bigquery/v1/tables/#{CGI.escape(table)}/imports/#{import_id}").and_return(successful_return)
+    import_status = BQ::ImportStatus.new(table,import_id)
   end
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -12,7 +12,6 @@ describe "Table" do
     ]
 
     data = {
-      :method => 'bigquery.tables.insert',
       :data => {:name => 'example_bucket/tables/my_pony_list', :fields => fields}
     }
     successful_response = {"result" => nil}
@@ -23,12 +22,8 @@ describe "Table" do
   end
 
   it "should permit delete table" do
-    data = {
-      :method => 'bigquery.tables.delete',
-      :params => {:name => 'table_name'}
-    }
     successful_response = {"result" => nil}
-    BQ.should_receive(:request).with(:delete, data.to_json, "https://www.googleapis.com/bigquery/v1/tables/#{CGI.escape('table_name')}").and_return(successful_response) 
+    BQ.should_receive(:request).with(:delete, {}, "https://www.googleapis.com/bigquery/v1/tables/#{CGI.escape('table_name')}").and_return(successful_response) 
 
     result = BQ::Table.delete('table_name')
     result.has_key?('result').should be_true
