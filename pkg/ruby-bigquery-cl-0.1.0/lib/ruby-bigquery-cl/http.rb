3cefd6e095dfcbcaafe28f3d566c5cade7ab4f6d
Cainã Costa <cainan.costa@gmail.com>
2011-04-05 13:35:42 -0300

    Removida a pasta pkg.

--- pkg/ruby-bigquery-cl-0.1.0/lib/ruby-bigquery-cl/http.rb
+++
@@ -1,8 +0,0 @@
-class Net::HTTP
-  alias_method :old_initialize, :initialize
-  def initialize(*args)
-    old_initialize(*args)
-    @ssl_context = OpenSSL::SSL::SSLContext.new
-    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  end
-end
