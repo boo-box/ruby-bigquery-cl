67a290408b76194f4ded7abc82fab99b75466e5c
Cainã Costa <cainan.costa@gmail.com>
2011-04-07 02:54:05 -0300

    Moved bq.rb to big_query.rb

--- lib/ruby-bigquery-cl.rb
+++ lib/ruby-bigquery-cl.rb
@@ -5,7 +5,7 @@ require 'cgi'
 require 'json'
 
 require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'http')
-require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'bq')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'big_query')
 require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'query')
 require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'import')
 require File.join(File.expand_path(File.dirname(__FILE__)), 'ruby-bigquery-cl', 'import_status')
