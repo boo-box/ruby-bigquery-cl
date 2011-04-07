e831d7c0f7732297011702753008c54152a06f82
Cainã Costa <cainan.costa@gmail.com>
2011-04-07 01:20:40 -0300

    Refactor on the Table class tests

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,31 +1,26 @@
 require 'spec_helper'
 
 describe "Table" do
+  let(:fields) { {} }
+  let(:data) { { :data => {:name => 'bucket/tables/my_pony_list', :fields => fields} } }
+  let(:successful_response) { { "result" => nil } }
 
-  it "should permit create table" do
-    fields = [
-      {"type"=> "string", "id"=> "name", "mode"=> "REQUIRED"},
-      {"type"=> "integer", "id"=> "age", "mode"=> "REQUIRED"},
-      {"type"=> "float", "id"=> "weight", "mode"=> "REQUIRED"},
-      {"type"=> "boolean", "id"=> "is_magic", "mode"=> "REQUIRED"}
-    ]
+  it "should create table" do
+    BigQuery.should_receive(:request).
+             with(:post, data.to_json, "https://www.googleapis.com/bigquery/v1/tables").
+             and_return(successful_response)
 
-    data = {
-      :data => {:name => 'example_bucket/tables/my_pony_list', :fields => fields}
-    }
-    successful_response = {"result" => nil}
-    BQ.should_receive(:request).with(:post, data.to_json, "https://www.googleapis.com/bigquery/v1/tables").and_return(successful_response) 
-
-    result = BQ::Table.create('example_bucket/tables/my_pony_list', fields)
-    result.has_key?('result').should be_true
+    result = BigQuery::Table.create('bucket/tables/my_pony_list', fields)
+    result.should have_key('result')
+    result['result'].should be_nil
   end
 
-  it "should permit delete table" do
-    successful_response = {"result" => nil}
-    BQ.should_receive(:request).with(:delete, {}.to_json, "https://www.googleapis.com/bigquery/v1/tables/#{CGI.escape('table_name')}").and_return(successful_response) 
+  it "should delete table" do
+    BigQuery.should_receive(:request).
+             with(:delete, "{}", "https://www.googleapis.com/bigquery/v1/tables/table_name").
+             and_return({})
 
-    result = BQ::Table.delete('table_name')
-    result.has_key?('result').should be_true
+    result = BigQuery::Table.delete('table_name')
+    result.should == {}
   end
-
-end 
+end
